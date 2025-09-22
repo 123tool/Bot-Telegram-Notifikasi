8e83825327aa0536472af6818c458364ef5b976f
123Tool <161654922+123tool@users.noreply.github.com>
2025-09-22 14:50:17 -0700

    Update notifikasi.sh

--- notifikasi.sh
+++ notifikasi.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
-CHATID="ID YOUR"
-TOKEN="TOKEN BOT"
-TIME="10"
-URL="https://api.telegram.org/bot$TOKEN/sendMessage"
-TEKS="Tes Notifikasi" 
+#!/bin/bash CHATID = "54321" TOKEN = "abcdEfGH1234567890" TIME = "10" URL =
 
 
 
+
+" https://api.telegram.org/bot$TOKEN/sendMessage "
+TEXT = "It's time to pray, Bro!" 
 curl -s --max-time $TIME -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEKS" $URL >/dev/null
+   
